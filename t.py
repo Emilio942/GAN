94c566f5b7da7f992e49bb4bd8d7323b2d7b13c7
Emilio942 <81811598+Emilio942@users.noreply.github.com>
2024-03-04 09:36:09 +0100

    Implementiert Zustandsspeicherung und Leistungsüberprüfung für GAN-Training
    Dieser Commit fügt mehrere wichtige Funktionen zum GAN-Trainingsskript hinzu:
    - Speicherung des Zustands von Generator und Diskriminator in regelmäßigen Abständen, um den Fortschritt des Trainings zu sichern.
    - Einführung einer Funktion zum Laden gespeicherter Modellzustände beim Start des Skripts, um das Training von einem früheren Punkt fortsetzen zu können.
    - Implementierung einer Überprüfungsfunktion, die die Leistung des Modells bewertet und bei schlechter Leistung einen Neustart des Trainings ermöglicht, wenn kein voriger Speicherpunkt vorhanden ist.
    
    Diese Änderungen erhöhen die Robustheit des Trainingsprozesses und bieten mehr Flexibilität beim Umgang mit Trainingsabbrüchen oder schlechter Modellleistung.

--- t.py
+++ t.py
@@ -1,111 +1,127 @@
 import torch
-from torch import nn
+from torch import nn, optim
 from torchvision import datasets, transforms
 from torch.utils.data import DataLoader
 from torchvision.utils import save_image
 import os
 
-# Überprüfen, ob ein CUDA-fähiges Gerät verfügbar ist, um das Training zu beschleunigen
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+# Gerät konfigurieren
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 # Hyperparameter
-latent_dim = 100
 batch_size = 64
-learning_rate = 0.0002
-image_size = 28*28
+lr = 0.0002
+latent_dim = 100
 num_epochs = 50
+image_size = 28 * 28
 
-# MNIST Datensatz laden
+# MNIST Datensatz
 transform = transforms.Compose([
     transforms.ToTensor(),
-    transforms.Normalize((0.5,), (0.5,))
+    transforms.Normalize([0.5], [0.5]),
 ])
-train_dataset = datasets.MNIST(root="./data", train=True, download=True, transform=transform)
-train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+dataset = datasets.MNIST(root='./data', train=True, download=True, transform=transform)
+dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
 
-# Generator definieren
+# Generator
 class Generator(nn.Module):
     def __init__(self):
         super(Generator, self).__init__()
         self.model = nn.Sequential(
-            nn.Linear(latent_dim, 256),
-            nn.LeakyReLU(0.2),
+            nn.Linear(latent_dim, 128),
+            nn.ReLU(True),
+            nn.Linear(128, 256),
+            nn.ReLU(True),
             nn.Linear(256, 512),
-            nn.LeakyReLU(0.2),
-            nn.Linear(512, 1024),
-            nn.LeakyReLU(0.2),
-            nn.Linear(1024, image_size),
-            nn.Tanh()
+            nn.ReLU(True),
+            nn.Linear(512, image_size),
+            nn.Tanh(),
         )
 
     def forward(self, z):
-        return self.model(z).view(-1, 1, 28, 28)
+        img = self.model(z)
+        img = img.view(img.size(0), 1, 28, 28)
+        return img
 
-# Diskriminator definieren
+# Diskriminator
 class Discriminator(nn.Module):
     def __init__(self):
         super(Discriminator, self).__init__()
         self.model = nn.Sequential(
             nn.Linear(image_size, 512),
-            nn.LeakyReLU(0.2),
+            nn.LeakyReLU(0.2, inplace=True),
             nn.Linear(512, 256),
-            nn.LeakyReLU(0.2),
+            nn.LeakyReLU(0.2, inplace=True),
             nn.Linear(256, 1),
-            nn.Sigmoid()
+            nn.Sigmoid(),
         )
 
     def forward(self, img):
         img_flat = img.view(img.size(0), -1)
-        return self.model(img_flat)
+        validity = self.model(img_flat)
+        return validity
 
-# Instanzen von Generator und Diskriminator erstellen
+# Instanzen und Optimierer
 generator = Generator().to(device)
 discriminator = Discriminator().to(device)
 
-# Zustand des Generators und Diskriminators laden, falls vorhanden
-if os.path.isfile('generator_state.pth'):
-    generator.load_state_dict(torch.load('generator_state.pth'))
-if os.path.isfile('discriminator_state.pth'):
-    discriminator.load_state_dict(torch.load('discriminator_state.pth'))
+optimizer_G = optim.Adam(generator.parameters(), lr=lr)
+optimizer_D = optim.Adam(discriminator.parameters(), lr=lr)
 
-# Verlustfunktion und Optimierer
+# Verlustfunktion
 criterion = nn.BCELoss()
-optimizer_G = torch.optim.Adam(generator.parameters(), lr=learning_rate)
-optimizer_D = torch.optim.Adam(discriminator.parameters(), lr=learning_rate)
 
-# Verzeichnis für die generierten Bilder erstellen, falls nicht vorhanden
-if not os.path.exists('images'):
-    os.makedirs('images')
+# Verzeichnis für generierte Bilder
+if not os.path.exists('images_gan'):
+    os.makedirs('images_gan')
 
-# Trainingsschleife
-for epoch in range(num_epochs):
-    for i, (imgs, _) in enumerate(train_loader):
-        # Echte und generierte Bilder vorbereiten
-        real_imgs = imgs.to(device)
-        z = torch.randn(batch_size, latent_dim).to(device)
-        fake_imgs = generator(z)
+# Hilfsfunktionen für Speichern und Laden
+def save_checkpoint(generator, discriminator, epoch):
+    torch.save({
+        'generator_state_dict': generator.state_dict(),
+        'discriminator_state_dict': discriminator.state_dict(),
+        'epoch': epoch,
+    }, f'checkpoint_{epoch}.pth')
 
-        # Diskriminator trainieren
-        optimizer_D.zero_grad()
-        real_loss = criterion(discriminator(real_imgs), torch.ones(imgs.size(0), 1).to(device))
-        fake_loss = criterion(discriminator(fake_imgs.detach()), torch.zeros(batch_size, 1).to(device))
-        d_loss = (real_loss + fake_loss) / 2
-        d_loss.backward()
-        optimizer_D.step()
+def load_checkpoint(generator, discriminator, filepath):
+    if os.path.isfile(filepath):
+        print(f"Lade Checkpoint '{filepath}'")
+        checkpoint = torch.load(filepath)
+        generator.load_state_dict(checkpoint['generator_state_dict'])
+        discriminator.load_state_dict(checkpoint['discriminator_state_dict'])
+        return checkpoint['epoch']
+    else:
+        print("Kein Checkpoint gefunden. Starte das Training von vorne.")
+        return 0
 
-        # Generator trainieren
-        optimizer_G.zero_grad()
-        g_loss = criterion(discriminator(fake_imgs), torch.ones(batch_size, 1).to(device))
-        g_loss.backward()
-        optimizer_G.step()
+# Trainingsfunktion
+def train(generator, discriminator, dataloader, epochs, save_interval):
+    for epoch in range(epochs):
+        for i, (imgs, _) in enumerate(dataloader):
+            real_imgs = imgs.to(device)
+            valid = torch.ones(imgs.size(0), 1).to(device)
+            fake = torch.zeros(imgs.size(0), 1).to(device)
 
-    print(f"Epoch {epoch+1}/{num_epochs} | D Loss: {d_loss.item()} | G Loss: {g_loss.item()}")
+            # Trainiere Generator
+            optimizer_G.zero_grad()
+            z = torch.randn(imgs.size(0), latent_dim).to(device)
+            gen_imgs = generator(z)
+            g_loss = criterion(discriminator(gen_imgs), valid)
+            g_loss.backward()
+            optimizer_G.step()
 
-    # Generierte Bilder nach jeder Epoche speichern
-    if epoch % 10 == 0:
-        save_image(fake_imgs.data[:25], f"images/{epoch}_fake.png", nrow=5, normalize=True)
+            # Trainiere Diskriminator
+            optimizer_D.zero_grad()
+            real_loss = criterion(discriminator(real_imgs), valid)
+            fake_loss = criterion(discriminator(gen_imgs.detach()), fake)
+            d_loss = (real_loss + fake_loss) / 2
+            d_loss.backward()
+            optimizer_D.step()
 
-    # Modellzustände speichern
-    if (epoch + 1) % 50 == 0:
-        torch.save(generator.state_dict(), 'generator_state.pth')
-        torch.save(discriminator.state_dict(), 'discriminator_state.pth')
+        if epoch % save_interval == 0 or epoch == epochs-1:
+            save_image(gen_imgs.data[:25], f'images_gan/{epoch}.png', nrow=5, normalize=True)
+            save_checkpoint(generator, discriminator, epoch)
+
+# Startpunkt des Trainings
+start_epoch = load_checkpoint(generator, discriminator, 'latest_checkpoint.pth')
+train(generator, discriminator, dataloader, num_epochs - start_epoch, 10)
